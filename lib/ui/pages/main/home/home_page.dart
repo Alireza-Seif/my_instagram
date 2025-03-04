22eccb9fc086ab851b8583376b3b873b1867c793
alireza <alirezaariye@gmail.com>
2025-03-04 21:15:10 +0330

    feat: Implement horizontal highlight list in home page

--- lib/ui/pages/main/home/home_page.dart
+++ lib/ui/pages/main/home/home_page.dart
@@ -1,10 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:my_instagram/model/fake_highlight_model.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    List<FakeHighlightModel> heightLights = [];
+
+    heightLights.add(FakeHighlightModel('Fall', 'assets/images/fall.jpg'));
+    heightLights.add(FakeHighlightModel('Spring', 'assets/images/spring.jpg'));
+    heightLights.add(FakeHighlightModel('Fall', 'assets/images/fall.jpg'));
+    heightLights.add(FakeHighlightModel('Spring', 'assets/images/spring.jpg'));
+    heightLights.add(FakeHighlightModel('Fall', 'assets/images/fall.jpg'));
+    heightLights.add(FakeHighlightModel('Spring', 'assets/images/spring.jpg'));
+    heightLights.add(FakeHighlightModel('Fall', 'assets/images/fall.jpg'));
+    heightLights.add(FakeHighlightModel('Spring', 'assets/images/spring.jpg'));
+    heightLights.add(FakeHighlightModel('Fall', 'assets/images/fall.jpg'));
+    heightLights.add(FakeHighlightModel('Spring', 'assets/images/spring.jpg'));
+    return Scaffold(
+      body: Column(
+        children: [
+          SizedBox(
+            height: 80,
+            width: Get.width ,
+            child: ListView.builder(
+                itemCount: heightLights.length,
+                scrollDirection: Axis.horizontal,
+                shrinkWrap: true,
+                itemBuilder: (context, index) {
+                  return Container(
+                    margin: const EdgeInsets.only(right: 5, top: 10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        ClipOval(
+                          child: Image.asset(
+                            heightLights.elementAt(index).icon!,
+                            width: 48,
+                            height: 48,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        Text(heightLights.elementAt(index).title!)
+                      ],
+                    ),
+                  );
+                }),
+          )
+        ],
+      ),
+    );
   }
 }
